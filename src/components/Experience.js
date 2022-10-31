43879ca7b302a6e3733d645b21a6a6df011598e3
pankeku <drwagw@gmail.com>
2022-10-31 12:14:36 +0200

    refactor

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,26 +1,35 @@
-import styles from "./Experience.module.css"
+import styles from './experience.module.css';
 
-function handleClick(e) {
-    
-}
+function handleClick(e) {}
 
 export default function Experience() {
-  return <div className={styles.container}>
-       <div className={styles.header_container}>
-    <h1>Experience</h1>
-    <div className="editButton" onClick={handleClick}>Edit</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header_container}>
+        <h1>Experience</h1>
+        <div className="editButton" onClick={handleClick}>
+          Edit
+        </div>
+      </div>
+
+      <label htmlFor="company">
+        Company name
+        <div className="company" id="company">
+          BBBB
+        </div>
+      </label>
+      <label htmlFor="position">
+        Position
+        <div className="position" id="position">
+          Eeeerker
+        </div>
+      </label>
+      <label htmlFor="date">
+        Date
+        <div className="date" id="date">
+          2000
+        </div>
+      </label>
     </div>
-
-    <label htmlFor="company"> Company name
-    <div className="company" id="company">BBBB</div>
-    </label>
-    <label htmlFor="position">Position
-    <div className="position" id="position">Eeeerker</div>
-    </label>
-    <label htmlFor="date">Date
-    <div className="date" id="date">2000</div>
-    </label>
-  </div>;
+  );
 }
-
-
--- src/components/General.js
+++ src/components/General.js
@@ -1,26 +1,34 @@
-import styles from "./General.module.css"
+import styles from './general.module.css';
 
-function handleClick(e) {
-    
-}
+function handleClick(e) {}
 
 export default function General() {
-  return <div className={styles.container}>
-    <div className={styles.header_container}>
-    <h1>General info</h1>
-    <div className="editButton" onClick={handleClick}>Edit</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header_container}>
+        <h1>General info</h1>
+        <div className="editButton" onClick={handleClick}>
+          Edit
+        </div>
+      </div>
+      <label htmlFor="name">
+        Name
+        <div className="name" id="name">
+          Eeeee
+        </div>
+      </label>
+      <label htmlFor="email">
+        E-mail
+        <div className="email" id="email">
+          email@email.com
+        </div>
+      </label>
+      <label htmlFor="phone">
+        Phone
+        <div className="phone" id="phone">
+          +0000000000
+        </div>
+      </label>
     </div>
-
-    <label htmlFor="name"> Name
-    <div className="name" id="name">Eeeee</div>
-    </label>
-    <label htmlFor="email">E-mail
-    <div className="email" id="email">email@email.com</div>
-    </label>
-    <label htmlFor="phone">Phone
-    <div className="phone" id="phone">+0000000000</div>
-    </label>
-  </div>;
+  );
 }
-
-
